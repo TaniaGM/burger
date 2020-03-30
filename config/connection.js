eff21fa5f45f54cd250e59226008e08bdc5f667b
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-03-29 21:31:57 -0400

    fixing errors

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if(process.env.JAWSDB_URL){
 else {
     connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
+    port: 3306,
     user: "root",
     password: "Cookie20",
     database: "burgers_db"
