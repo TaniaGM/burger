71903c297b9a0fd3a7a1849516d652dff18bcd25
Tania Moore <tania@Tanias-MacBook-Pro.local>
2020-04-01 17:10:06 -0400

    update server

--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-bur// Import the ORM to create functions that will interact with the database.
+ bur// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
